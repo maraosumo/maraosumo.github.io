1a7d99c074e6dc037af9a69a533a9e4db976ba8c
maraosumo <mdao2@byu.edu>
2025-01-29 23:10:31 -0700

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -239,18 +239,18 @@
     .addTo(map);
 
 //KML for UT County
- var kmlLayer = omnivore.kml('Utah County Major_LayerToKML.kml') // Replace with your file path
+ var kmlLayer2 = omnivore.kml('Utah County Major_LayerToKML.kml') // Replace with your file path
             .on('ready', function() {
- // var layer2 = kmlLayer.getLayers();
+ var layer2 = kmlLayer.getLayers();
         
- //        layer2.forEach(function(feature) {
- //            var props2 = feature.feature.properties;
+        layer2.forEach(function(feature) {
+            var props2 = feature.feature.properties;
             
- //            // Bind popup with name or description from KML
- //            feature.bindPopup(props2.name || "No Name");
+            // Bind popup with name or description from KML
+            feature.bindPopup(props2.name || "No Name");
             
- //            // Bind tooltip (label) with name
- //            feature.bindTooltip(props2.name || "Unnamed", { permanent: true, direction: "right" });
- //        });
+            // Bind tooltip (label) with name
+            feature.bindTooltip(props2.name || "Unnamed", { permanent: true, direction: "right" });
+        });
         })
     .addTo(map);
