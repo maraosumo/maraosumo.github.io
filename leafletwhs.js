d4c01201fbe5940b91302d256b5c74227496ebcc
maraosumo <mdao2@byu.edu>
2025-02-05 20:37:34 -0700

    Update leafletwhs.js

--- leafletwhs.js
+++ leafletwhs.js
@@ -1,1 +1,5 @@
+var map = L.map("map").setView([40.7608, -111.8910], 10) // View Centered on SLC
 
+// Add a tile layer (e.g., OpenStreetMap)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map)
