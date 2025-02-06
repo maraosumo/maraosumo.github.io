0b7ffc48fb00e5fd7e50d831eab49a0532bbf27b
maraosumo <mdao2@byu.edu>
2025-02-05 21:16:41 -0700

    Update leafletwhs.js

--- leafletwhs.js
+++ leafletwhs.js
@@ -7,21 +7,21 @@ var wmsLayer = L.tileLayer.wms('https://ows.mundialis.de/services/service?', {
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map)
 
-var wmsLayer1 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-d96feb82dd71498e8be30dc2b4fb8d02/wms', {
+const wmsLayer1 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-d96feb82dd71498e8be30dc2b4fb8d02/wms', {
       layers: 'HS-d96feb82dd71498e8be30dc2b4fb8d02:Counties',
 	transparent: 'true',
 	format: 'image/png',
       attribution: 'Hydroshare GeoServer'
 	}).addTo(map);
 
-var wmsLayer2 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-d96feb82dd71498e8be30dc2b4fb8d02/wms', {
+const wmsLayer2 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-d96feb82dd71498e8be30dc2b4fb8d02/wms', {
       layers: 'HS-d96feb82dd71498e8be30dc2b4fb8d02:UTA_Routes_and_Most_Recent_Ridership',
 	transparent: 'true',
 	format: 'image/png',
       attribution: 'Hydroshare GeoServer'
 	}).addTo(map);
 
-var wmsLayer3 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-d96feb82dd71498e8be30dc2b4fb8d02/wms', {
+const wmsLayer3 = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-d96feb82dd71498e8be30dc2b4fb8d02/wms', {
       layers: 'HS-d96feb82dd71498e8be30dc2b4fb8d02:UTA_Stops_and_Most_Recent_Ridership',
 	transparent: 'true',
 	format: 'image/png',
