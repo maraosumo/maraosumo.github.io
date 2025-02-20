9a34036c4e5d253e1b466749803327d19813bc78
maraosumo <mdao2@byu.edu>
2025-02-19 18:50:06 -0700

    Update riverphoto.js

--- riverphoto.js
+++ riverphoto.js
@@ -2,7 +2,9 @@ var map = L.map("map").setView([45.32028874603523, -116.53680470840834], 10) // 
 
 // Add a tile layer (e.g., OpenStreetMap)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { 
-  attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',}).addTo(map)
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map)
 
 var kmlLayer = omnivore.kml('Idaho Rivers_LayerToKML.kml') // Replace with your file path
 .on('ready', function() {
